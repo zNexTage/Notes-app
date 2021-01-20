857384c18b9d61fa226ed9d9f983843b06710a42
Christian Oliveira <christianoliveira1908@hotmail.com>
2021-01-20 19:01:59 -0300

    handlers para os campos e requisição para o backend

--- Frontend/notes-app/src/App.tsx
+++ Frontend/notes-app/src/App.tsx
@@ -1,25 +1,103 @@
-import React from 'react';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Input from './Components/Input';
+import RealizingNotes from './Assets/39019-completing-tasks.json';
+import LottiePlayer from 'lottie-web';
+import Lottie from './Components/Lottie';
+import { Container } from 'react-bootstrap'
+import NoteIcon from './Components/NoteIcon';
+import MySvg from './Assets/41070-notepad-with-a-list-of-tick-boxes-and-5-star-feedback.json';
+import Button from './Components/Button'
+import LoginClient from './Client/Login';
+import { gql, useMutation } from '@apollo/client';
 
 function App() {
+  const LOGIN_MUTATION = gql`
+  mutation login($username:String!, $password:String!){
+      Login(username:$username, password:$password){
+          id
+          name
+      }
+  }    
+  
+  `;
+
+  const [login, { data }] = useMutation(LOGIN_MUTATION);
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value)
+  }
+
+  const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  }
+
+  const handlebtnLogin = () => {
+    login({
+      variables: {
+        username, password
+      }
+    }).then((value) => {
+      console.log(value)
+    })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   return (
-    <div className="login-container">
-      <div className="login-content">
-        <label>
-          Nome usuário
-      </label>
-        <br />
-        <input
-          type="text" />
-        <br />
-        <label>
-          Senha
-      </label>
-        <br />
-        <input
-          type="password" />
+    <Container className="container-background" fluid>
+      <div className="login-container">
+
+        <Lottie
+          id="lottie-animation"
+          autoplay={true}
+          loop={true}
+          animationData={MySvg}
+          width="50%"
+        />
+        <div className="login-content">
+          <div className="login-header">
+            <h1>
+              Notes App
+          </h1>
+            <NoteIcon
+              width={60}
+              height={60}
+            />
+          </div>
+          <br />
+          <Input
+            placeholder="Nome de usuário"
+            type="text"
+            onChange={handleUsername}
+          />
+
+          <br />
+          <Input
+            placeholder="Senha"
+            type="password"
+            onChange={handlePassword}
+          />
+          <br />
+          <div className="login-buttons">
+            <Button
+              onClick={handlebtnLogin}
+              color="#E88BCF"
+              title="Entrar"
+            />
+            <Button
+              color="#ECA7FF"
+              title="Cadastrar"
+            />
+          </div>
+        </div>
+
       </div>
-    </div>
+    </Container>
   );
 }
 
