c307d78e1484831204267adac794cef54eed6933
Christian de Oliveira <christianoliveira1908@hotmail.com>
2021-02-04 18:00:25 -0300

    Model com os atributos do usuário.

---
+++ Frontend/notes-app/src/Model/User.ts
@@ -0,0 +1,12 @@
+class User {
+    id:number;
+    name:string;
+    lastname:string;
+    username:string;
+    password:string;
+    createdAt:Date;
+    updatedAt:Date;
+    picture:string;
+}
+
+export default User;
