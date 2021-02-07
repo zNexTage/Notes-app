e0178434492b8d55bcc6d84971c0931a9d632e63
Christian de Oliveira <christianoliveira1908@hotmail.com>
2021-02-06 21:53:00 -0300

    Troquei a extensão do arquivo

--- Frontend/notes-app/src/Api/index.ts
+++ Frontend/notes-app/src/Api/index.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
     uri: 'http://192.168.0.162:4000',
-    cache: new InMemoryCache()
+    cache: new InMemoryCache()    
 });
 
 export default client;
