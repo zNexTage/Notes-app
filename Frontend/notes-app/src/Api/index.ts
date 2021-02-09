c89d53481e24feaa0a9aa8d4b3369dcc52b0115c
Christian Oliveira <christianoliveira1908@hotmail.com>
2021-02-09 18:00:16 -0300

    Utilizi o createHttpLink do apollo para definir o endpoint

--- Frontend/notes-app/src/Api/index.ts
+++ Frontend/notes-app/src/Api/index.ts
@@ -1,8 +1,20 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { createHttpLink } from "@apollo/client/link/http";
+
+const link = createHttpLink({ uri: "http://192.168.0.12:4000" });
+
 
 const client = new ApolloClient({
-    uri: 'http://192.168.0.162:4000',
-    cache: new InMemoryCache()    
+    link,
+    cache: new InMemoryCache(),
+    defaultOptions: {
+        query: {
+            errorPolicy: "all"
+        },
+        mutate: {
+            errorPolicy: 'all'
+        }
+    }
 });
 
 export default client;
