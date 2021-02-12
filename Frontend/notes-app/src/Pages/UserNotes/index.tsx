61057161fa47a24b4aa145c9737c6c4ba1feecac
Christian Oliveira <christianoliveira1908@hotmail.com>
2021-02-12 15:57:56 -0300

    Implementei as funções de atualizar e remover a nota na tela usernotes.

--- Frontend/notes-app/src/Pages/UserNotes/index.tsx
+++ Frontend/notes-app/src/Pages/UserNotes/index.tsx
@@ -1,32 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import NoteCard from '../../Components/NoteCard';
 import _ from 'lodash';
-import { gql } from '@apollo/client';
 import Loading from '../../Components/Loading';
 import User from '../../Model/User';
 import UserUtil from '../../Util/UserUtil';
 import { useHistory } from 'react-router-dom';
 import "./style.css";
 import Button from '../../Components/Button';
-import client from '../../Api';
 import Note from '../../Model/Note';
 import NoNotesRegistered from '../../Components/NoNotesRegistered';
+import NoteClient from '../../Client/Note.client';
+import NoteModal, { TypeModal } from '../../Components/Modal/NoteModal';
+import YesNoModal from '../../Components/Modal/YesNoModal';
+import Modals from '../../Enum/Modals';
+import FullscreenAnimation, { FullscreenAnimationOptions } from '../../Components/FullscreenAnimation';
 
-const GET_NOTES = gql`
-    query NotesByUser($idUser:Int!) {
-        NotesByUser(idUser: $idUser){
-            id
-            title
-            content
-            createdAt
-        }
-    }
-`;
+import RemoveNoteAnimation from '../../Assets/remove_note.json';
+import UpdateNoteAnimation from '../../Assets/update_notes.json';
+import ErrorAnimation from '../../Assets/error-animation.json';
+
 
 function UserNotes() {
     const history = useHistory();
+    const [loggedUser, setLoggedUser] = useState<User>();
     const [isLoading, setIsLoading] = useState<boolean>(false);
-    const [listNotes, setNotes] = useState<Array<Note>>([]);
+    const [listNotes, setListNotes] = useState<Array<Note>>([]);
+    const [showModal, setShowModal] = useState<Modals>(Modals.NONE);
+    const [noteToUpdate, setNoteToUpdate] = useState<Note>();
+    const [noteToDelete, setNoteToDelete] = useState<Note>();
+    const [fullscreenAnimationOptions, setFullscreenAnimationOptions] = useState<FullscreenAnimationOptions>();
+    const [showStatusAnimation, setShowStatusAnimation] = useState<boolean>(false);
 
     useEffect(() => {
         const userUtil = new UserUtil();
@@ -41,47 +44,163 @@ function UserNotes() {
 
         setIsLoading(true);
 
-        client.query({
-            query: GET_NOTES,
-            variables: {
-                idUser: user.id
-            }
-        }).then((response) => {
-            setNotes(response.data.NotesByUser);
+        setLoggedUser(user);
+
+        NoteClient.getUserNotes(user.id).then((notes) => {
+            setListNotes(notes);
         }).catch((err) => {
-            console.log(err);
-        }).finally(()=>{
+            history.replace('error');
+        }).finally(() => {
             setIsLoading(false);
         });
     }, []);
 
-    return (
-        <div className="notes-container notes-container-align">
-            {isLoading && <Loading isLoading={isLoading} />}
-            {!isLoading && (
-                <>
-                    <Button
-                        onClick={() => history.goBack()}
-                        title="Voltar"
-                        color="#C055C9" />
-                    <br />
-                    {!_.isEmpty(listNotes) && listNotes.map((note: Note) => (
-                        <NoteCard
-                            onRemoveButtonClick={()=>{
-                                
-                            }} 
-                            onUpdateButtonClick={()=>{
+    const handlerNotes = ({ note, isNewNote }: { note: Note, isNewNote: boolean }) => {
+        updateNote(note);
+    }
 
-                            }}
-                            key={`notecard___${note.id}`}
-                            note={note} />
-                    ))}
-                    {_.isEmpty(listNotes) &&
-                        <NoNotesRegistered />
-                    }
-                </>
-            )}
-        </div>
+    const updateNote = (noteToUpdate: Note) => {
+        const userId = loggedUser!.id;
+
+        setIsLoading(true);
+
+        NoteClient.updateNote(noteToUpdate.id, {
+            noteContent: noteToUpdate.content,
+            noteTitle: noteToUpdate.title,
+            createdAt: noteToUpdate.createdAt,
+            userId
+        }).then((updatedNote) => {
+            //Atualiza no array de notas a nota que foi alterada pelo usuário.
+            const updatedListNotes = listNotes.map((note) => {
+                if (note.id === noteToUpdate.id) {
+                    return updatedNote;
+                }
+
+                return note;
+            });
+
+            setListNotes([...updatedListNotes]);
+
+            NoteClient.updateNotesCache([...updatedListNotes], userId);
+
+            setFullscreenAnimationOptions({
+                animation: UpdateNoteAnimation,
+                color: "#5FE378",
+                text: `Sua nota foi atualizada com sucesso!`,
+                width: '50%'
+            });
+
+            setShowModal(Modals.NONE);
+            setNoteToUpdate(undefined);
+        }).catch((err) => {
+            setFullscreenAnimationOptions({
+                animation: ErrorAnimation,
+                color: "#963041",
+                text: "Ops! Não foi possível atualizar a sua nota :("
+            });
+        }).finally(() => {
+            setIsLoading(false);
+            setShowStatusAnimation(true);
+        });
+    }
+
+    const deleteNote = (idNote: number) => {
+        setIsLoading(true);
+
+        NoteClient.deleteNote(idNote).then(({ id }) => {
+            const updatedListNotes = listNotes.filter((note) => note.id !== id);
+
+            setListNotes([...updatedListNotes]);
+
+            NoteClient.updateNotesCache([...updatedListNotes], loggedUser!.id);
+
+            setFullscreenAnimationOptions({
+                animation: RemoveNoteAnimation,
+                color: "#5FE378",
+                text: `Sua nota foi removida com sucesso!`,
+                animationTime: 3600
+            });
+
+            setShowModal(Modals.NONE);
+            setNoteToDelete(undefined);
+        }).catch((err) => {
+            setFullscreenAnimationOptions({
+                animation: ErrorAnimation,
+                color: "#963041",
+                text: "Ops! Não foi possível remover a sua nota :("
+            });
+        }).finally(() => {
+            setIsLoading(false);
+            setShowStatusAnimation(true);
+        })
+    }
+
+    return (
+        <>
+            <div className="user-notes-container">
+                <Button
+                    onClick={() => history.goBack()}
+                    title="Voltar"
+                    color="#C055C9" />
+
+                <div className="notes-container">
+                    {isLoading && <Loading isLoading={isLoading} />}
+                    {!isLoading && (
+                        <>
+                            {!_.isEmpty(listNotes) && listNotes.map((note: Note) => (
+                                <NoteCard
+                                    onRemoveButtonClick={() => {
+                                        setNoteToDelete(note);
+                                        setShowModal(Modals.YES_NO_MODAL);
+                                    }}
+                                    onUpdateButtonClick={() => {
+                                        setNoteToUpdate(note);
+                                        setShowModal(Modals.NOTES_MODAL);
+                                    }}
+                                    key={`notecard___${note.id}`}
+                                    note={note} />
+                            ))}
+                            {_.isEmpty(listNotes) &&
+                                <NoNotesRegistered />
+                            }
+
+                            <NoteModal
+                                key={`${showModal}`}
+                                noteToUpdate={noteToUpdate}
+                                onConfirm={handlerNotes}
+                                showModal={showModal === Modals.NOTES_MODAL}
+                                typeModal={TypeModal.UPDATE}
+                                onClose={() => {
+                                    setNoteToUpdate(undefined);
+                                    setShowModal(Modals.NONE)
+                                }} />
+
+                            <YesNoModal
+                                showModal={showModal === Modals.YES_NO_MODAL}
+                                onYesButtonClick={() => {
+                                    deleteNote(noteToDelete!.id);
+                                }}
+                                onNoButtonClick={() => {
+                                    setNoteToDelete(undefined);
+                                    setShowModal(Modals.NONE);
+                                }}
+                            />
+
+                        </>
+
+                    )}
+
+
+                </div>
+            </div>
+            {showStatusAnimation &&
+                <FullscreenAnimation
+                    options={fullscreenAnimationOptions!}
+                    onAnimationCompleted={() => {
+                        setShowStatusAnimation(false)
+                        setFullscreenAnimationOptions({ animation: "", color: "", text: "" });
+                    }} />}
+        </>
     )
 }
 
