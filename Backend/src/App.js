c6155ea21997904ca26d40f359e9ca341221721f
Christian de Oliveira <christianoliveira1908@hotmail.com>
2021-02-12 21:01:57 -0300

    habilitei o cors

--- Backend/src/App.js
+++ Backend/src/App.js
@@ -2,7 +2,7 @@ const {ApolloServer} = require('apollo-server');
 const typeDefs = require('./GraphQl')
 const resolvers = require('./GraphQl/Resolvers');
     
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers, cors: true });
     
 server.listen().then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
